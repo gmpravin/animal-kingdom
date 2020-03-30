599333bad709566ebc1a703006f29d5591b80d49
gmpravin <40927922+gmpravin@users.noreply.github.com>
2020-03-30 06:05:35 +0530

    Added

---
+++ build/precache-manifest.89a6930408a9951f67613ddeeacfbf13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edfc17a3a3a28a80a2cd9411bf7b026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8ecd2c253dbad28b8b5",
+    "url": "/static/css/2.19d11308.chunk.css"
+  },
+  {
+    "revision": "74f6f7cd23050db9f681",
+    "url": "/static/css/main.eb882395.chunk.css"
+  },
+  {
+    "revision": "e8ecd2c253dbad28b8b5",
+    "url": "/static/js/2.30f58361.chunk.js"
+  },
+  {
+    "revision": "74f6f7cd23050db9f681",
+    "url": "/static/js/main.39377321.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
